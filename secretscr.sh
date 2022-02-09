3b76e19a32c186eda1f93f32a17c2fc1a4539bfd
Max <44694269+MaximVI@users.noreply.github.com>
2022-02-09 11:09:17 +0500

    Create secretscr.sh

---
+++ secretscr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Enter IP"
+read ipaddress
+ssh root@$ipaddress 'echo "Free disk space"; df -h | grep "/dev/*"; echo "The five largest directories"; du -Sh /var/www/*/data/www/* | sort -rh | head -5; echo "The five largest databases"; du -Sh /var/lib/mysql/* --exclude=/var/lib/mysql/ib* --exclude=/var/lib/mysql/aria* --exclude=/var/lib/mysql/multi* --exclude=/var/lib/mysql/roundcube* --exclude=/var/lib/mysql/*.log --exclude=/var/lib/mysql/performance_schema* --exclude=/var/lib/mysql/mysql* | sort -rh | head -5; echo "Check license key bitrix"; cat /var/www/*/data/www/*/bitrix/license_key.php | grep LICENSE'
